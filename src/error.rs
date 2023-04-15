d0f6224f7903bf120a3ca3df348419837aa8c557
Ferdia McKeogh <ferdia@mckeogh.tech>
2023-04-15 16:55:14 +0100

    Calendar endpoint working

--- src/error.rs
+++ src/error.rs
@@ -11,12 +11,12 @@ use {
 /// Route error, presented to user through `IntoResponse` impl
 #[derive(Debug, displaydoc::Display, thiserror::Error)]
 pub enum Error {
-    /// Unknown location name {0:?}
-    UnknownLocationName(String),
     /// File {0:?} not found
     FileNotFound(String),
     /// Database error
     DatabaseError(#[from] sqlx::Error),
+    /// Invalid group format
+    InvalidGroupFormat(String),
 }
 
 impl IntoResponse for Error {
@@ -24,7 +24,7 @@ impl IntoResponse for Error {
         error!("Error occurred when handling request: {}", self);
 
         let status_code = match self {
-            Error::UnknownLocationName(_) => StatusCode::BAD_REQUEST,
+            Error::InvalidGroupFormat(_) => StatusCode::BAD_REQUEST,
             Error::FileNotFound(_) => StatusCode::NOT_FOUND,
             Error::DatabaseError(_) => StatusCode::INTERNAL_SERVER_ERROR,
         };
--- src/routes/calendar.rs
+++ src/routes/calendar.rs
@@ -8,45 +8,49 @@ use {
         },
         response::IntoResponse,
     },
+    chrono::{DateTime, Utc},
     ics::{
         escape_text,
-        properties::{Categories, Description, DtEnd, DtStart, Organizer, Status, Summary},
+        properties::{Categories, Description, DtEnd, DtStart, Organizer, Summary},
         Event, ICalendar,
     },
+    once_cell::sync::Lazy,
+    regex::Regex,
     sqlx::{Pool, Postgres},
-    std::collections::HashSet,
+    uuid::Uuid,
 };
 
+static GROUP_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new("^[0-9a-z]+$").unwrap());
+
 pub async fn calendar(
-    State(_): State<Pool<Postgres>>,
+    State(db): State<Pool<Postgres>>,
     Path(groups): Path<String>,
 ) -> Result<impl IntoResponse, Error> {
-    dbg!(groups.split('+').collect::<HashSet<_>>());
+    // retrieve requested event groups from path
+    let groups = groups
+        .split('+')
+        .map(|s| match GROUP_REGEX.is_match(s) {
+            true => Ok(s.to_owned()),
+            false => Err(Error::InvalidGroupFormat(groups.clone())),
+        })
+        .collect::<Result<Vec<_>, Error>>()?;
 
     // for each group, pull all it's events from db, put into ical and return
+    let events = sqlx::query_as!(
+        DbEvent,
+        r#"SELECT * FROM events WHERE "group" = ANY($1)"#,
+        &groups
+    )
+    .fetch_all(&db)
+    .await?;
+
+    let mut calendar = ICalendar::new("2.0", "khronos");
+
+    events
+        .iter()
+        .for_each(|event| calendar.add_event(event.into()));
 
     let mut body = vec![];
-
-    let mut calendar = ICalendar::new("2.0", "ics-rs");
-
-    let mut event = Event::new("b68378cf-872d-44f1-9703-5e3725c56e71", "19960704T120000Z");
-    // add properties
-    event.push(Organizer::new("mailto:jsmith@example.com"));
-    event.push(DtStart::new("19960918T143000Z"));
-    event.push(DtEnd::new("19960920T220000Z"));
-    event.push(Status::confirmed());
-    event.push(Categories::new("CONFERENCE"));
-    event.push(Summary::new("Networld+Interop Conference"));
-    // Values that are "TEXT" must be escaped (only if the text contains a comma,
-    // semicolon, backslash or newline).
-    event.push(Description::new(escape_text(
-        "Networld+Interop Conference and Exhibit\n\
-         Atlanta World Congress Center\n\
-         Atlanta, Georgia",
-    )));
-    // add event to calendar
-    calendar.add_event(event);
-
     calendar.write(&mut body).unwrap();
 
     let mut headers = HeaderMap::new();
@@ -57,4 +61,45 @@ pub async fn calendar(
     );
 
     Ok((headers, body))
+}
+
+#[derive(Debug)]
+struct DbEvent {
+    name: String,
+    email: String,
+    description: String,
+    group: String,
+    start: DateTime<Utc>,
+    end: DateTime<Utc>,
+}
+
+impl<'a> From<&'a DbEvent> for Event<'a> {
+    fn from(
+        DbEvent {
+            name,
+            email,
+            description,
+            group,
+            start,
+            end,
+        }: &'a DbEvent,
+    ) -> Self {
+        let mut event = Event::new(
+            Uuid::new_v4().as_hyphenated().to_string(),
+            format_datetime(start),
+        );
+
+        event.push(Summary::new(name));
+        event.push(Description::new(escape_text(description)));
+        event.push(Organizer::new(format!("mailto:{email}")));
+        event.push(DtStart::new(format_datetime(start)));
+        event.push(DtEnd::new(format_datetime(end)));
+        event.push(Categories::new(group));
+
+        event
+    }
+}
+
+fn format_datetime(datetime: &DateTime<Utc>) -> String {
+    datetime.format("%Y%m%dT%H%M%SZ").to_string()
 }
